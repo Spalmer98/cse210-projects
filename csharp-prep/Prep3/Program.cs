a577e374e0f440b93eee73f072380c28fadecc92
Spalmer98 <monkies989@gmail.com>
2024-09-21 15:13:22 -0600

    Completed the Prep 3 Requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int randomNumber;
+        int guess;
+        string userInput;
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            /*Console.Write("\nWhat is your magic number? ");
+            userInput = Console.ReadLine();
+            randomNumber = int.Parse(userInput);*/
+
+            Random randomGenerator = new Random();
+            randomNumber = randomGenerator.Next(1, 100);
+
+            do
+            {
+                
+
+                Console.Write("\nWhat is your guess? ");
+                userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("\nHigher\n");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("\nLower\n");
+                }
+            } while (guess != randomNumber);
+
+            Console.WriteLine("\nYou guessed it!\n");
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
