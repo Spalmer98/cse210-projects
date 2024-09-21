79593a3818b32dd7e3ad5510f9f8da3f66a94129
Spalmer98 <monkies989@gmail.com>
2024-09-21 14:56:05 -0600

    Completed the requirements for Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter;
+        string userInput;
+
+        Console.Write("\nPlease enter your grade. ");
+        userInput = Console.ReadLine();
+        grade = int.Parse(userInput);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";;
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!!\n");
+        }
+        else 
+        {
+            Console.WriteLine("You didn't pass, better luck next time.");
+        }
     }
 }
