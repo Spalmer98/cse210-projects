9f8157859933564564ee0827d017c4ba37f5dc1c
Spalmer98 <monkies989@gmail.com>
2024-09-29 23:27:45 -0600

    Got the write portion of the menu to properly call the AddEntry function from the Journal class and got the Display option to display the current journal entries.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,74 +1,47 @@
 using System;
 
-public class Journal
-{
-    static List<Entry> _entries = new List<Entry>();
-    static void AddEntry(string newEntry)
-    {
-
-    }
-
-    static void DisplayAll()
-    {
-
-    }
-
-    static void SaveToFile()
-    {
-
-    }
-
-    static void LoadFromFile()
-    {
-
-    }
-}
-
-public class Entry
-{
-    public static string _entryText;
-    public static string _date;
-    static string _promptText = JournalPrompts.GetRandomPrompt();
-
-    public static void Display()
-    {
-        Console.WriteLine(_promptText);
-        _entryText = Console.ReadLine();
-        _date = DateTime.Now.ToString("MM/dd/yyyy");
-    }
-    
-
-}
-
-public class JournalPrompts
-{
-    static List<string> _prompts = new List<string>{"Did I learn anything new today?", 
-                                                   "What was the hardest thing I did today?", 
-                                                   "Did I do any acts of service today, and if so what was it?", 
-                                                   "What's a goal I can make to be better tomorrow?", 
-                                                   "What was different about today?"};
-    public static string GetRandomPrompt()
-    {
-        Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.Next(0, 4);
-        return _prompts[randomNumber];
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
+        Journal newJournal = new Journal();
+        int _userChoice = 0;
+
         Console.WriteLine("\nWelcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5.Quit\n");
-        Console.Write("What would you like to do? ");
-        int _userChoice = int.Parse(Console.ReadLine());
 
-        if (_userChoice == 1)
+        do
         {
-            Entry.Display();
-        }
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+            Console.Write("What would you like to do? ");
+            _userChoice = int.Parse(Console.ReadLine());
 
+            if (_userChoice == 1)
+            {
+                Entry newEntry = new Entry();
+                newJournal.AddEntry(newEntry);
+            }
+            else if (_userChoice == 2)
+            {
+                newJournal.DisplayAll();
+            }
+            else if (_userChoice == 3)
+            {
+
+            }
+            else if (_userChoice == 4)
+            {
+
+            }
+            else if (_userChoice == 5)
+            {
+                Console.WriteLine("Have a wonderful day!");
+            }
+            else
+            {
+                Console.WriteLine("You did not enter in a correct option, Please try again.");
+            }
+
+        } while (_userChoice != 5);
     }
 }
